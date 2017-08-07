bd7127be245f46f472507d1819632071248652e6
Brian Anstett <baanstet@oakland.edu>
2017-08-07 09:50:50 -0400

    Print board method

--- source/edu/oakland/production/display/TTTDisplay.java
+++ source/edu/oakland/production/display/TTTDisplay.java
@@ -21,8 +21,15 @@ public class TTTDispaly {
 		
 		
 	}
-	public boolean printCurrentBoard(){
-		
+	public void  printCurrentBoard(){
+		//Get array from  middleware, return 3d array
+		char[][] b;//getboard from middleware.
+	        System.out.println("\t  1 2 3");
+	        System.out.println("\tA "+ b[0][0] + "|" +b[0][1] + "|" + b[0][2]);
+	        System.out.println("\t -------");
+	        System.out.println("\tB "+ b[1][0] + "|" +b[1][1] + "|" + b[1][2]);
+	        System.out.println("\t -------");
+	        System.out.println("\tC "+ b[2][0] + "|" +b[2][1] + "|" + b[2][2]);	
 	}
 	
 	public void changePlayer(char mark){
